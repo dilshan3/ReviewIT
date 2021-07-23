35678d2907837bee735eac859ed820aebc71f05b
Dilshan Prasad <dilshan.prasad@sl.iec.local>
2021-07-23 16:29:13 +0530

    another commit

--- ReviewApp/src/environments/environment.prod.ts
+++ ReviewApp/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiBaseUrl: '',
-  omdbApiUrl: 'http://www.omdbapi.com/?apiKey=',
+  omdbApiUrl: '//www.omdbapi.com/?apiKey=',
   omdbApiKey: 'd7a3dfb8',
   sentimUrl: 'https://sentim-api.herokuapp.com/api/v1/'
 };
